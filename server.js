5725dc163c934f4da8c45ceef4c2ba2537e62342
Yasas <yasaskulathunga.y.r.b.k@gmail.com>
2020-12-10 21:45:19 +0530

    add_addproducts&editproduct

--- server.js
+++ server.js
@@ -4,13 +4,15 @@ import config from './config';
 import dotenv from 'dotenv';
 import mongoose  from 'mongoose';
 import userRoute from './routes/userRoute'
+import productRoute from './routes/productRoute'
+import bodyParser from 'body-parser' 
 
 dotenv.config();
 
 const mongodbUrl = config.MONGODB_URL;
 //start create database connection
 mongoose.connect(mongodbUrl,{ 
-    useNewUrlParser:true,
+    useNewUrlParser:true,  
     useCreateIndex:true,
     useUnifiedTopology:true,
     
@@ -19,20 +21,22 @@ mongoose.connect(mongodbUrl,{
 
 const app =express();
 
+app.use(bodyParser.json({limit: '1mb'}));  
 app.use("/api/users", userRoute);
+app.use("/api/products", productRoute);
 
-app.get("/api/products/:id", (req, res) => {
-    const productId = req.params.id;
-    const product = data.products.find( x => x._id === productId);
-    if(product)
-    res.send(product);
-    else 
-    res.status(404).send({ msg:"Product Not Found"})
-});
+// app.get("/api/products/:id", (req, res) => {
+//     const productId = req.params.id;
+//     const product = data.products.find( x => x._id === productId);
+//     if(product)
+//     res.send(product);
+//     else 
+//     res.status(404).send({ msg:"Product Not Found"})
+// });
 
 
-app.get("/api/products", (req, res)=>{
-    res.send(data.products);
-});
+// app.get("/api/products", (req, res)=>{
+//     res.send(data.products);
+// });
 
 app.listen(5000,() =>{console.log("Server is at http://localhost:5000")} )
